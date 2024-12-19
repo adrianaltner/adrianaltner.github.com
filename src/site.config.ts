a81bcabb3983e31b4d31357a1f0370a2ef97d950
Adrian Altner <altner@mailbox.org>
2024-12-19 03:38:26 +0100

    Initial commit

---
+++ src/site.config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+	name: 'Adrian Altner',
+	origin: 'https://adrianaltner.com',
+	basePathname: '/',
+	title: 'Adrian Altner',
+	description: 'Full-Time Multi-Hyphenater & Enthusiast of the Ascetic Lifestyle',
+	blogPostsPerPage: 10,
+};
