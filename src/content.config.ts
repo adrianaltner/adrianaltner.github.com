2328d3c851d3d9c44fa163cc5906ded43e03a6ee
Adrian Altner <altner@mailbox.org>
2024-12-23 02:01:54 +0100

    Add: PostPreview

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,13 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, reference, z } from 'astro:content';
 
+const baseSchema = z.object({
+	title: z.string().max(60),
+});
 
 const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) => z.object({
-		title: z.string(),
+	loader: glob({ base: './src/content/blog', pattern: '**/[^_]*.{md,mdx}' }),
+	schema: ({ image }) => baseSchema.extend({
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
@@ -17,22 +19,18 @@ const blog = defineCollection({
 });
 
 const categories = defineCollection({
-	loader: glob({ base: './src/content/categories', pattern: '**/*.{md,mdx}' }),
-	schema: z.object({
-		title: z.string(),
+	loader: glob({ base: './src/content/categories', pattern: '**/[^_]*.{md,mdx}' }),
+	schema: ({ image }) => baseSchema.extend({
 		description: z.string(),
-		// TODO: Add support for images
-		// image: z.string().optional(),
+		image: image().optional(),
 	}),
 });
 
 const tags = defineCollection({
-	loader: glob({ base: './src/content/tags', pattern: '**/*.{md,mdx}' }),
-	schema: z.object({
-		title: z.string(),
+	loader: glob({ base: './src/content/tags', pattern: '**/[^_]*.{md,mdx}' }),
+	schema: ({ image }) => baseSchema.extend({
 		description: z.string(),
-		// TODO: Add support images
-		// image: z.string().optional(),
+		image: image().optional(),
 	}),
 });
 
